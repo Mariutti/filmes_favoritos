1de1011a4e0e4062d401e35c8749a921f853ec32
Pedro Alencar <pedromariutti@gmail.com>
2023-03-02 19:44:32 -0300

    Giving style

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// document.querySelector('form').addEventListener('submit', (event)=> event.preventDefault())
+
 class Movie {
   title;
   score;
@@ -38,31 +40,85 @@ const locadora = [
   },
 ];
 
+listarFilmes(locadora);
 const btnCadastrar = document.querySelector("#btnCadastrar");
 btnCadastrar.addEventListener("click", cadastrar);
 
-function cadastrar(){
-    event.preventDefault();
-    let movieTitle = document.querySelector("#title");
-    let score = document.querySelector("#score").value;
-    let movieLength = document.querySelector("#movieLength").value;
-    if(locadora.map((movie)=>(movie.title)).includes(movieTitle.value)){
-        console.log("repetido");
-        movieTitle.value = '';
-        score = '';
-        movieLength = '';
-        console.log(locadora);
-    }else{
-        console.log("novo");
-        const movie = new Movie(movieTitle.value, score, movieLength);
-        console.log(movie);
-        locadora.push(movie);
-        console.log(locadora);
-        movieTitle.value = '';
-        score = '';
-        movieLength = '';
-    }
-
+function cadastrar() {
+  event.preventDefault();
+  let movieTitle = document.querySelector("#title");
+  let score = document.querySelector("#score").value;
+  let movieLength = document.querySelector("#movieLength").value;
+  if (
+    locadora
+      .map((movie) => movie.title.toLowerCase())
+      .includes(movieTitle.value.toLowerCase())
+  ) {
+    console.log("repetido");
+    alert("Filme já cadastrado");
+    movieTitle.value = "";
+    score = "";
+    movieLength = "";
+    console.log(locadora);
+  } else {
+    console.log("novo");
+    const movie = new Movie(movieTitle.value, score, movieLength);
+    console.log(movie);
+    locadora.push(movie);
+    console.log(locadora);
+    movieTitle.value = "";
+    score = "";
+    movieLength = "";
+  }
+  listarFilmes(locadora);
 }
 
+function listarFilmes(array) {
+  let i = 1;
+  const cardsUl = document.querySelector(".cardsUl");
+  cardsUl.innerHTML = null;
 
+  array.forEach((item) => {
+    console.log(`Filme ${i}`);
+    const liCard = document.createElement("li");
+    liCard.className = "liCard";
+    const ulCardProp = document.createElement("ul");
+    ulCardProp.className = "ulCardProp";
+
+    item.id = i;
+    for (const value in item) {
+      if (value === "title" || value === "score" || value === "movieLength") {
+        console.log(value + ": " + item[value]);
+        const li = document.createElement("li");
+        li.innerHTML = `${value}: ${item[value]}`;
+
+        li.className = "liProp";
+        ulCardProp.appendChild(li);
+      }
+    }
+    console.log("-------------------");
+    i++;
+    liCard.appendChild(ulCardProp);
+    cardsUl.appendChild(liCard);
+  });
+}
+
+const searchInput = document.querySelector("#searchInput");
+searchInput.addEventListener("search", buscar);
+
+function buscar() {
+  let finder = searchInput.value;
+
+  console.log(finder);
+  let findArray = [];
+  if ((finder = "")) {
+    findArray = locadora;
+  }
+  // const findArray = ()=>locadora.map(movie => movie.title.toLowerCase()).filter((title)=> title.includes(finder.toLowerCase()))
+
+  console.log(findArray);
+  const cardsUl = document.querySelector(".cardsUl");
+  cardsUl.innerHTML = null;
+  listarFilmes(findArray);
+}
+window.addEventListener("onload", buscar);
