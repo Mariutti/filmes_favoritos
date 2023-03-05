050e177a4a58661198990cd45b1854c1725ae958
Pedro Alencar <pedromariutti@gmail.com>
2023-03-04 22:38:06 -0300

    Try to add counter fav

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 class Movie {
   title;
   score;
@@ -39,7 +38,7 @@ const locadora = [
   },
 ];
 
-listarFilmes(locadora)
+listarFilmes(locadora);
 const btnCadastrar = document.querySelector("#btnCadastrar");
 btnCadastrar.addEventListener("click", cadastrar);
 
@@ -64,78 +63,128 @@ function cadastrar() {
     movieTitle.value = "";
     score = "";
     movieLength = "";
-    locadora.push(movie)
+    locadora.push(movie);
   }
   listarFilmes(locadora);
 }
 
 function listarFilmes(array) {
   const cardsUl = document.querySelector(".cardsUl");
-  cardsUl.innerHTML = null;
+  cardsUl.innerHTML = "";
 
   array.forEach((item) => {
     const liCard = document.createElement("li");
     liCard.className = "liCard";
     const ulCardProp = document.createElement("ul");
     ulCardProp.className = "ulCardProp";
-    const heartImg = document.createElement('img');
-    heartImg.className = 'heartImg'
-    heartImg.src = 'https://icons.iconarchive.com/icons/paomedia/small-n-flat/256/heart-icon.png'
+    const heartImg = document.createElement("img");
+    heartImg.classList = "heartImg heartImgOpac";
+    heartImg.src =
+      "https://icons.iconarchive.com/icons/paomedia/small-n-flat/256/heart-icon.png";
 
     // item.id = i;
     for (const value in item) {
       if (value === "title" || value === "score" || value === "movieLength") {
-
         const li = document.createElement("li");
         li.innerHTML = `${value}: ${item[value]}`;
 
         li.className = "liProp";
+        if (value === "title") {
+          li.classList.add("title");
+        } else if (value === "score") {
+          li.classList.add("score");
+        } else if (value === "movieLength") {
+          li.classList.add("movieLength");
+        }
         ulCardProp.appendChild(li);
       }
-    ulCardProp.appendChild(heartImg)
-    liCard.appendChild(ulCardProp);
-    cardsUl.appendChild(liCard);
-    };
+      ulCardProp.appendChild(heartImg);
+      liCard.appendChild(ulCardProp);
+      cardsUl.appendChild(liCard);
+    }
   });
 }
 
 const searchInput = document.querySelector("#searchInput");
-const searchSection = document.querySelector('.searchSection')
-const text = document.createElement('p')
-searchSection.appendChild(text)
-searchInput.addEventListener("keyup", seekMovie)
-  let finderArray = []
+const searchSection = document.querySelector(".searchSection");
+const text = document.createElement("p");
+searchSection.appendChild(text);
+searchInput.addEventListener("keyup", seekMovie);
+let finderArray = [];
 
 function seekMovie(e) {
-  e.preventDefault() 
-  const searchTextValue = e.target.value
-  text.innerHTML = searchTextValue  
-  
-  if(!searchTextValue){
-    finderArray = locadora.map(item=>item) 
-  }else{
-    finderArray = []
-    finderArray = 
-      locadora.map(movie=>{
-      if(movie.title.toLowerCase().includes(searchTextValue.toLowerCase())){
-        return movie
+  e.preventDefault();
+  const searchTextValue = e.target.value;
+  text.innerHTML = searchTextValue;
+
+  if (!searchTextValue) {
+    finderArray = locadora.map((item) => item);
+  } else {
+    finderArray = [];
+    finderArray = locadora.map((movie) => {
+      if (movie.title.toLowerCase().includes(searchTextValue.toLowerCase())) {
+        return movie;
       }
-    })
+    });
   }
-  
-  listarFilmes(finderArray)
-  heartToggling()
+  const advertiseH2 = document.createElement("h2");
+  advertiseH2.className = "advertiseH2";
+  listarFilmes(finderArray);
+  checarFinder(finderArray);
+}
+
+function heartToggling() {
+  const heartImg = document.querySelectorAll(".heartImg");
+  const heartToggle = heartImg.forEach((img) => {
+    
+    img.addEventListener("click", (e) => {
+      "strict mode";
+      e.preventDefault()
+      const favorite = ()=>{
+        let tituloDaImg = e.target.parentNode.firstChild.innerText.slice(7)
+        locadora.forEach((movie)=>{
+          if(movie.title === tituloDaImg){
+            if(movie.favorite == false){
+              movie.favorite = true
+            }else{
+              movie.favorite = false
+            }
+          }
+        })
+        finderArray.forEach((movie)=>{
+          if(movie.title === tituloDaImg){
+            if(movie.favorite == false){
+              movie.favorite = true
+            }else{
+              movie.favorite = false
+            }
+          }
+        })
+      }
+      favorite()
+      img.classList.toggle("heartImgOpac");
+    });
+    console.log(finderArray)
+  });
+ 
+}
+heartToggling();
+
+const checarFinder = (vetor) => {
+  const advertiseH2 = document.createElement("h2");
+  advertiseH2.className = "advertiseH2";
+  if (vetor.every((item) => item == undefined)) {
+    console.log("teste");
+    const cardsSection = document.querySelector(".cardsSection");
+    document.querySelector(".cardsUl").style.display = "none";
+
+    advertiseH2.innerText =
+      "Não foi encontrado nenhum filme com esse critério de busca.";
+    cardsSection.appendChild(advertiseH2);
+  } else {
+    document.querySelector(".cardsUl").style.display = "flex";
+    const advertiseH2 = document.querySelector(".advertiseH2");
+    advertiseH2.remove();
+  }
+  listarFilmes(vetor);
 };
-
-
-function heartToggling(){
-const heartImg = document.querySelectorAll('.heartImg')
-const heartToggle = heartImg.forEach(img =>{
-  img.addEventListener('click', (e)=>{
-    'stric mode'
-    img.classList.toggle('heartImgOpac')
-  })
-}
-                )
-}
-heartToggling()
